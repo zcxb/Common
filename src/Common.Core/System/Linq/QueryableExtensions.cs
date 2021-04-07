510d262125c8a93ffabf439e2da9efc02e2fe6af
chenjunyao <chen13157379235@outlook.com>
2021-04-07 09:35:03 +0800

    update

---
+++ src/Common.Core/System/Linq/QueryableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Common.Linq
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
+        {
+            return condition ? query.Where(predicate) : query;
+        }
+    }
+}
