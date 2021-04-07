87afb017afa480fc9117976038eab80b9b74e33c
chenjunyao <chen13157379235@outlook.com>
2021-04-07 14:48:45 +0800

    update

--- src/Common.Application.Contracts/Dtos/EntityDto.cs
+++ src/Common.Application.Contracts/Dtos/EntityDto.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Common.Application.Dtos
 {
     public class EntityDto<TKey>
     {
+        [JsonPropertyName("id")]
         public TKey Id { get; set; }
     }
 }
