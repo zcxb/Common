1acf58782aa69fe64a9bf43aa725cb8368fe7a04
chenjunyao <chen13157379235@outlook.com>
2021-04-07 11:21:04 +0800

    add application dtos

---
+++ src/Common.Application.Contracts/Dtos/EntityDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Application.Dtos
+{
+    public class EntityDto<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
---
+++ src/Common.Application.Contracts/Dtos/PagerDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Application.Dtos
+{
+    public class PagerDto
+    {
+        public int Page { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
