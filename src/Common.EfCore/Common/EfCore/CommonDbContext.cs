b8e4cfeb44d013c47476289723508449b2e4bc5e
chenjunyao <chen13157379235@outlook.com>
2021-04-13 14:53:48 +0800

    update

---
+++ src/Common.EfCore/Common/EfCore/CommonDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.EfCore
+{
+    public abstract class CommonDbContext<TDbContext> : DbContext, IEfCoreDbContext
+        where TDbContext : DbContext
+    {
+        protected CommonDbContext(DbContextOptions<TDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
