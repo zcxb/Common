e8d88667b8ebdf23704dd4d3142e834bb11b82c6
chenjunyao <chen13157379235@outlook.com>
2021-04-14 15:47:43 +0800

    update

---
+++ src/Common.Core/Common/Modularity/IModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Modularity
+{
+    public interface IModule
+    {
+        void ConfigureServices();
+    }
+}
---
+++ src/Common.Core/Common/Modularity/ModuleBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Modularity
+{
+    public abstract class ModuleBase : IModule
+    {
+        public virtual void ConfigureServices()
+        {
+        }
+    }
+}
