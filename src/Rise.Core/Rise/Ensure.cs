07a4f51cfca307e62371783de3ca859674678340
zcxb <chen13157379235@outlook.com>
2021-04-17 22:11:12 +0800

    update

---
+++ src/Rise.Core/Rise/Ensure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Rise
+{
+    [DebuggerStepThrough]
+    public static class Ensure
+    {
+        public static T NotNull<T>(T value, string parameterName, string message = null)
+        {
+            return value ?? throw new ArgumentNullException(parameterName, message);
+        }
+    }
+}
