7e533f3e88ebcdb2f35e058d4b24a6e5902d0695
zcxb <chen13157379235@outlook.com>
2021-09-02 22:15:10 +0800

    add efcore extensions

---
+++ src/Rise.EfCore/Rise/EfCore/EfCoreExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rise.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rise.EfCore
+{
+    public static class EfCoreExtensions
+    {
+        public static IQueryable<TEntity> GetById<TEntity>(this IQueryable<TEntity> query, int id)
+            where TEntity : AuditedModelBase
+        {
+            return query.Where(i => i.Id == id);
+        }
+
+        public static void RemoveRange<TEntity>(this DbSet<TEntity> set, int[] keyValues)
+           where TEntity : AuditedModelBase
+        {
+            var entities = set.Where(i => keyValues.Contains(i.Id));
+            set.RemoveRange(entities);
+        }
+    }
+}
