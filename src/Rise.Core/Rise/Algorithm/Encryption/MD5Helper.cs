fa3034c3370a0a6102fcb63ff187750e87e8b18a
chenjunyao <chen13157379235@outlook.com>
2021-09-06 14:21:37 +0800

    add md5 helper

---
+++ src/Rise.Core/Rise/Algorithm/Encryption/MD5Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Rise.Algorithm.Encryption
+{
+    public class MD5Helper
+    {
+        public static string ComputeHash(string str)
+        {
+            return ComputeHash(str, Encoding.Default);
+        }
+
+        public static string ComputeHash(string str, Encoding encoding)
+        {
+            using MD5 md5 = MD5.Create();
+            var bytes = encoding.GetBytes(str);
+            var hashBytes = md5.ComputeHash(bytes);
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+    }
+}
